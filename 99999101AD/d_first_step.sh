368f4022762454390222969fe9ad939caa6b2ff1
Pascal Andy <pascal@publiodigital.com>
2016-03-10 08:43:34 -0500

    99999101AD wip

--- 99999101AD/d_first_step.sh
+++ 99999101AD/d_first_step.sh
@@ -11,11 +11,14 @@ cd ghost && pwd && echo Here are the file we just cloned && ls -la &&
 docker-compose up -d
 
 # New config as our git repo containes ALL our configs
-_DEPLOYMENT_ID="99999101AD" && echo _DEPLOYMENT_ID &&
-_USR_BRANCH="pascal" && echo _USR_BRANCH &&
-_GIT_CLONE_URL="https://github.com/pascalandy/gst-maria-dock_fork-mewm.git" && echo _GIT_CLONE_URL &&
+_DEPLOYMENT_ID="99999101AD" &&
+_USR_BRANCH="pascal" &&
+_GIT_CLONE_URL="https://github.com/pascalandy/gst-maria-dock_fork-mewm.git" &&
+echo "The ENV _DEPLOYMENT_ID value is $_DEPLOYMENT_ID" &&
+echo "The ENV _USR_BRANCH value is $_USR_BRANCH" &&
+echo "The ENV _GIT_CLONE_URL value is $_GIT_CLONE_URL" &&
 cd /var/www/ && echo "cd into '$ /var/www/'" &&
-sudo mkdir $_DEPLOYMENT_ID && echo "Creating folder $_DEPLOYMENT_ID" &&
+sudo mkdir $_DEPLOYMENT_ID && echo "Folder $_DEPLOYMENT_ID created." &&
 sudo chmod 777 -R /var/www/$_DEPLOYMENT_ID &&
 cd $_DEPLOYMENT_ID && echo "cd into '$ /var/www/$_DEPLOYMENT_ID" &&
 sudo git clone -b $_USR_BRANCH $_GIT_CLONE_URL tmp &&
