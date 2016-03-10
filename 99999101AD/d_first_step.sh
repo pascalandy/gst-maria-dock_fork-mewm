00e95f9450e450a6a39d693c516d34b90adb5dab
Pascal Andy <pascal@publiodigital.com>
2016-03-10 08:28:14 -0500

    99999101AD d_first_step wip

--- 99999101AD/d_first_step.sh
+++ 99999101AD/d_first_step.sh
@@ -1,16 +1,29 @@
-_DEPLOYMENT_ID="99999101AD" &&
-echo _DEPLOYMENT_ID &&
-_GitCloneUrl="https://github.com/pascalandy/gst-maria-dock_fork-mewm.git" &&
-echo _GitCloneUrl &&
-cd /var/www/ &&
-echo Creating the directory for $_DEPLOYMENT_ID &&
+_DEPLOYMENT_ID="99999101AD" && echo _DEPLOYMENT_ID &&
+_USR_BRANCH="pascal" && echo _USR_BRANCH &&
+_GIT_CLONE_URL="https://github.com/pascalandy/gst-maria-dock_fork-mewm.git" && echo _GIT_CLONE_URL &&
+cd /var/www/ && echo Creating the directory for $_DEPLOYMENT_ID &&
 sudo mkdir $_DEPLOYMENT_ID &&
-sudo chmod 777 -R /var/www/* &&
+sudo chmod 777 -R /var/www/ &&
 cd $_DEPLOYMENT_ID &&
-echo $PATH
-echo Cloning the configs (branch $_DEPLOYMENT_ID) &&
-sudo git clone -b $_DEPLOYMENT_ID $_GitCloneUrl ghost &&
+echo "Cloning the configs from branch $_USR_BRANCH" &&
+sudo git clone -b $_USR_BRANCH $_GIT_CLONE_URL ghost &&
+cd ghost && pwd && echo Here are the file we just cloned && ls -la &&
+docker-compose up -d
+
+# New config as our git repo containes ALL our configs
+_DEPLOYMENT_ID="99999101AD" && echo _DEPLOYMENT_ID &&
+_USR_BRANCH="pascal" && echo _USR_BRANCH &&
+_GIT_CLONE_URL="https://github.com/pascalandy/gst-maria-dock_fork-mewm.git" && echo _GIT_CLONE_URL &&
+cd /var/www/ && echo "cd into '$ /var/www/'" &&
+sudo mkdir $_DEPLOYMENT_ID && echo "Creating folder $_DEPLOYMENT_ID" &&
+sudo chmod 777 -R /var/www/$_DEPLOYMENT_ID &&
+cd $_DEPLOYMENT_ID && echo "cd into '$ /var/www/$_DEPLOYMENT_ID" &&
+sudo git clone -b $_USR_BRANCH $_GIT_CLONE_URL tmp &&
+sudo cp -r tmp/$_DEPLOYMENT_ID $_DEPLOYMENT_ID &&
+sudo rm -r tmp/ &&
+sudo mv $_DEPLOYMENT_ID ghost &&
+sudo chmod 777 -R /var/www/$_DEPLOYMENT_ID/ghost/ &&
 cd ghost &&
-echo Display the file we just cloned
-la -la &&
+ls -la && echo "Here are the file we just cloned for $_DEPLOYMENT_ID" &&
+echo "Ready for deployment!"
 docker-compose up -d
